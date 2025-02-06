8293b7563ba942bfac5d283558ab73ebc69277a5
Nathaniel Hayman <nathanielhayman@gmail.com>
2025-02-05 18:37:15 -0600

    add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+        {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/private/',
+        },
+        {
+            userAgent: 'Googlebot',
+            allow: ['/'],
+            disallow: '/private/',
+        },
+    ],
+    sitemap: 'https://acme.com/sitemap.xml',
+  }
+}
