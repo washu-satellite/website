c3b6ab8fa7a33e557d3b672b9173a0bde2295395
nathanielhayman <nathanielhayman@gmail.com>
2026-01-15 00:32:15 -0700

    meta branding update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -35,7 +35,7 @@ export const Route = createRootRouteWithContext<{
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'WashU Satellite',
       },
     ],
     links: [
