4072d988da6783c55267ad299dbce21c319e6df9
nathanielhayman <nathanielhayman@gmail.com>
2026-01-16 14:10:01 -0700

    fix build reqs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: './src/lib/db/schema',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL!,
   },
 });
