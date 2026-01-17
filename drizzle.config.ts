4912235eb94923a3031b00fb694589cb3509a9be
nathanielhayman <nathanielhayman@gmail.com>
2026-01-16 17:40:28 -0700

    use neon as db endpoint instead

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,11 +12,11 @@ export default defineConfig({
   schema: './src/lib/db/schema',
   dialect: 'postgresql',
   dbCredentials: {
-    // url: process.env.POSTGRES_URL!,
+    url: process.env.POSTGRES_URL!,
     host: process.env.POSTGRES_HOST!,
     user: process.env.POSTGRES_USER!,
     password: process.env.POSTGRES_PASSWORD!,
     database: process.env.POSTGRES_DATABASE!,
-    ssl: ca ? { ca: ca, rejectUnauthorized: true } : undefined,
+    // ssl: ca ? { ca: ca, rejectUnauthorized: true } : undefined,
   },
 });
